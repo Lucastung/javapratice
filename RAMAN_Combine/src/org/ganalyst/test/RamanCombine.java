108d51a8a30169f83377d5cf12cfec752e678119
lucastung <tong.lucas@gmail.com>
2020-11-18 11:58:26 +0800

    Lesson1-4 Merge file

--- RAMAN_Combine/src/org/ganalyst/test/RamanCombine.java
+++ RAMAN_Combine/src/org/ganalyst/test/RamanCombine.java
@@ -3,7 +3,6 @@ package org.ganalyst.test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -40,7 +39,7 @@ public class RamanCombine {
 		mFiles[1] = new File[mFiles[0].length];
 		for (int i=0 ;i< mFiles[0].length;i++) {
 			
-			File f1 = new File(atable.get("-T")+mFiles[0][i].getName());
+			File f1 = new File(atable.get("-C")+mFiles[0][i].getName());
 			if(f1.exists()) {
 				mFiles[1][i]=f1;
 			} else {
@@ -53,7 +52,8 @@ public class RamanCombine {
 		//******* Merge file list
 		try {
 			String result = fileMerge (mFiles);
-			Files.write(Paths.get(flist.get(0)), result.getBytes());
+			System.out.println(result);
+			//Files.write(Paths.get(flist.get(0)), result.getBytes());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -63,10 +63,36 @@ public class RamanCombine {
 
 	}
 
-	private static String fileMerge(File[][] mFiles) {
-		String result= null;
+	private static String fileMerge(File[][] mFiles) throws IOException {
+		// create container for result by using first file
+		String[] firstdata = new String(Files.readAllBytes(mFiles[0][0].toPath())).split("\n");
+		
+		String header = "WaveLength";
+		String[] data=new String[firstdata.length];
+		for(int i=0; i<firstdata.length;i++) {
+			data[i] = firstdata[i];
+		}
+		
+		//read and merged file
+		for (int i=0 ;i< mFiles[0].length;i++) {
+			String[] tdata = new String(Files.readAllBytes(mFiles[0][i].toPath())).split("\n");
+			String[] cdata = new String(Files.readAllBytes(mFiles[1][i].toPath())).split("\n");
+			//check section
 
-		return result;
+			//add sample name in header
+			header += "\t" + mFiles[0][i].getName().replaceAll(".txt", "_T")+"\t"+ mFiles[0][i].getName().replaceAll(".txt", "_C");
+			//add sample value to data container
+			for(int j=0;j<tdata.length;j++) {
+				data[j] = data[j]+ "\t" + tdata[j].split("\t")[1].trim()+"\t"+cdata[j].split("\t")[1].trim();
+			}
+		}
+		// combine result
+		StringBuilder sb = new StringBuilder(header+"\n");
+		for (int i=0;i<data.length;i++) {
+			sb.append("\n"+data[i]);
+		}
+		
+		return sb.toString();
 	}
 
 }

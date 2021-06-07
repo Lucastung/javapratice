1b464cf526c12d94a13c5dd6d592642a3a81d5a5
user <user@LAPTOP-2OHV88C9>
2021-06-08 00:17:19 +0800

    Anita add Hello for test

---
+++ Hello/src/Hey.java
@@ -0,0 +1,9 @@
+public class Hey   //定義物件  class, 物件的名字需與檔名一致!規定
+{  
+	public static void main(String[] args) {   //程式的起始點, "String[] args"->命令列參數
+		
+		String aaa = "lion";    //String 是一個物件
+		
+		System.out.println(aaa);  //字串需用""包起來, 每行程式後須加上; 已示結束
+	}
+}

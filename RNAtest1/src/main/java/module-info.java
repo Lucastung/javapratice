a687bb46c5980297b04188c439c3bfaaccc4a482
hsujc <hsujc@hsujc-ubuntu>
2021-06-09 17:03:15 +0800

    commit test

--- RNAtest1/src/main/java/module-info.java
+++ RNAtest1/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.gapp.hsujc.RNAtest1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
     opens org.gapp.hsujc.RNAtest1 to javafx.fxml;
     exports org.gapp.hsujc.RNAtest1;

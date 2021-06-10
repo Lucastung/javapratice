38e95e0db313ab92f2dd2042e8299b70c8d27356
user <user@LAPTOP-2OHV88C9>
2021-06-10 10:57:31 +0800

    delete test

--- RNAtest1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.gapp.hsujc.RNAtest1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.base;
-
-    opens org.gapp.hsujc.RNAtest1 to javafx.fxml;
-    exports org.gapp.hsujc.RNAtest1;
-}

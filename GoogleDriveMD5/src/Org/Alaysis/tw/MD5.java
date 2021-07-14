82153b99c9a9a56281a2d2823ec05306d9ba0b21
lincat <lincat@DESKTOP-TFATI2N>
2021-07-14 12:56:29 +0800

    讀取檔案

--- GoogleDriveMD5/src/Org/Alaysis/tw/MD5.java
+++ GoogleDriveMD5/src/Org/Alaysis/tw/MD5.java
@@ -1,32 +1,30 @@
 package Org.Alaysis.tw;
-import java.awt.List;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 
 public class MD5 {
 	public static void main(String[] args) throws IOException {	
 		String stringLine;
-		int num =0;
-		List<String>array = new ArrayList<String>();
 		File file = new File("report/5769.R720.ER");
-		InputStreamReader reader = new InputStreamReader(new FileInputStream(file));
-		BufferedReader brfferedreader = new BufferedReader(reader);
-		while(brfferedreader != null) {
-			num = (brfferedreader.indexOf("MD5 =");
-					
-										}
-		System.out.print(num);
-
+        File outfile = new File("report/test.txt") ;
+        FileWriter fw = new FileWriter (outfile);
+  BufferedWriter bw = new BufferedWriter(fw);
+  
+  InputStreamReader reader = new InputStreamReader(
+    new FileInputStream(file));
+  BufferedReader br = new BufferedReader(reader);
+  boolean firstLine = true ;
+  while ((stringLine=br.readLine())!=null) {
+   System.out.println(stringLine);
 																}  
+	} 
 			
 
 	}

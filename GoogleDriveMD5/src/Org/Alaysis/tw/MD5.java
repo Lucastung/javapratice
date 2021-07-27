90cf09d8d8d5824ee90ad9b2e68b252f704bcb5c
lincat <lincat@DESKTOP-TFATI2N>
2021-07-27 23:38:54 +0800

    抓取檔案

--- GoogleDriveMD5/src/Org/Alaysis/tw/MD5.java
+++ GoogleDriveMD5/src/Org/Alaysis/tw/MD5.java
@@ -9,19 +9,44 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 
 public class MD5 {
+	
 	public static void main(String[] args) throws IOException {	
 		//建立 stringLine
-		String stringLine;
-		//讀取檔案
-		File file = new File("report/5769.R720.ER");
+		String stringLine,uploadLine;
+		//讀取MD5檔案
+		File file = new File("report/7679.R720.ER");
+		//讀取檔案大小相關資料
+		File files = new File("report/uploadfilelist.txt");
 		//寫出檔案
 		File outfile = new File("report/test.txt") ;
         FileWriter fw = new FileWriter (outfile);
         //寫入讀取內容
-
+        fw.write("filepathway"+"\t\t\t\t\t\t\t\t\t\t\t"+ "DATA NAME"+"\n");
+        HashSet<String> uploadsites = new HashSet<String>();
+        HashSet<String> MD5sites = new HashSet<String>();
+        try (BufferedReader brs = new BufferedReader( new FileReader(files))){
+        	while((uploadLine=brs.readLine())!=null) {
+        String [] beginIndex = uploadLine.split("/");
+        String [] datasize = uploadLine.replaceAll("  ", " ").split(" ");
+//        System.out.println(datasize[4]+"\t"+beginIndex[1]);
+		
+        uploadsites.add(beginIndex[1]);
+        
+        	}
+//        	System.out.println(uploadsites);
+        	
+//        	System.out.println(rawData);
+        }
+        
+        
         try (BufferedReader br = new BufferedReader( new FileReader(file))) {	
         	while ((stringLine=br.readLine())!=null) {
         	//	   System.out.println(stringLine);
@@ -34,9 +59,12 @@ public class MD5 {
         				String [] MD5data = stringLine.split(":");
         				//		System.out.println(MD5data[3]+MD5data[4]);
         				fw.write(MD5data[3]+"\t"+MD5data[4]+"\n");
-
-					}
+        				MD5sites.add(MD5data[3]);
+//        				System.out.println(MD5data[3]);
+					}        		
         		}
+       	System.out.println(MD5sites);
+        	
 			fw.flush();
 			fw.close();
         }	 
